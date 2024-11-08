2491d2fabb5ea34a52850358e188f027c6be5442
Jude McCulley <grandmasterjude@gmail.com>
2024-11-08 17:05:10 -0600

    Add files via upload
    Ate a lot of cereal before bed.

--- weight-script.js
+++ weight-script.js
@@ -3,7 +3,7 @@ google.charts.setOnLoadCallback(drawChart);
 
 function average() {
     let average = document.getElementById("average");
-    let result = (241.8 + 241.4 + 241.2 + 241.4 + 241.8) / 5;
+    let result = (241.4 + 241.2 + 241.4 + 241.8 + 242.6) / 5;
     console.log('average :', average);
     console.log('result :', result);
     average.innerHTML = Math.round(result * 100) / 100;
@@ -78,7 +78,7 @@ function drawChart() {
         ['5 Nov 2024', 242.2],
         ['6 Nov 2024', 241.4],
         ['7 Nov 2024', 241.8],
-        ['8 Nov 2024', 0],
+        ['8 Nov 2024', 242.6],
         ['9 Nov 2024', 0],
         ['10 Nov 2024', 0],
         ['11 Nov 2024', 0],
@@ -198,7 +198,7 @@ function drawChart() {
         ['5 Nov 2024', 28.2],
         ['6 Nov 2024', 28.1],
         ['7 Nov 2024', 28.2],
-        ['8 Nov 2024', 0],
+        ['8 Nov 2024', 28.3],
         ['9 Nov 2024', 0],
         ['10 Nov 2024', 0],
         ['11 Nov 2024', 0],
@@ -318,7 +318,7 @@ function drawChart() {
         ['5 Nov 2024', 28.3, 35.5, 26.6],
         ['6 Nov 2024', 28.2, 35.7, 26.7],
         ['7 Nov 2024', 28.3, 35.6, 26.6],
-        ['8 Nov 2024', 0.0, 0.0, 0.0],
+        ['8 Nov 2024', 28.5, 35.4, 26.5],
         ['9 Nov 2024', 0.0, 0.0, 0.0],
         ['10 Nov 2024', 0.0, 0.0, 0.0],
         ['11 Nov 2024', 0.0, 0.0, 0.0],
